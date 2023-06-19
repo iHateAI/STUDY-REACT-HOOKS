4d1273ae01c6a26742f94c3423360f96aed95e42
HyunSeong <bolton7425@gmail.com>
2023-06-20 04:48:54 +0900

    useBeforeleave
    - 커서가 브라우저 밖으로 나갈 시

---
+++ useBeforeLeave.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+import { useState, useEffect, useRef } from "react";
+
+const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  }, []);
+};
+
+const App = () => {
+  const begForLife = () => console.log("Pls dont leave");
+  useBeforeLeave(begForLife);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
