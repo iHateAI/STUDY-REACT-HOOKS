bb774a14a5949c68b1d5eef6c076ed2ee582e107
HyunSeong <bolton7425@gmail.com>
2023-06-19 18:39:55 +0900

    onConfirm

---
+++ useConfirm.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+import { useState, useEffect, useRef } from "react";
+
+const useConfirm = (message, onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+  if (onCancel && typeof onCancel !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
+};
+
+const App = () => {
+  const deleteWorld = () => console.log("Deleting the world");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld);
+  return (
+    <div className="App">
+      <button>Delete the world</button>
+    </div>
+  );
+};
+
+export default App;
