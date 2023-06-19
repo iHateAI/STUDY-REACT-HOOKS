cc78cd1b9b1d7d578cdfa815623a4d84b2a918be
HyunSeong <bolton7425@gmail.com>
2023-06-20 05:37:49 +0900

    useAxios

---
+++ useAxios_APP.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { useState, useEffect, useRef } from "react";
+import useAxios from "./useAxios";
+
+const App = () => {
+  const { loading, error, data, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json"
+  });
+
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+};
+
+export default App;
