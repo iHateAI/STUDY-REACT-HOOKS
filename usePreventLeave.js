c6f1f6d5f79deb40bc384e058b2984ca5f66fdcd
HyunSeong <bolton7425@gmail.com>
2023-06-19 18:48:06 +0900

    usePreventLeave
    브라우저 종료 시 확인 메세지

---
+++ usePreventLeave.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import { useState, useEffect, useRef } from "react";
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
+};
+
+const App = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>unProtect</button>
+    </div>
+  );
+};
+
+export default App;
